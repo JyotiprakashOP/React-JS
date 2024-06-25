cfafd3e5d5f3754428ef5b6012c505344692502a
JyotiprakashOP <jyotiprakash24263@gmail.com>
2024-06-25 14:54:26 +0530

    props ongoing

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Navbar from './ClassAndFunctComponents';
 
 function App() {
+  // let user={name:"johnny" ,age:21,address:'blr'}
   return (
     
     <div>
-      <h1>Hello this is app</h1>
+      <h1>Hello this is app.js</h1>
       {/* <First></First> */}
-      <Navbar />
+      <Navbar name={"johnny"} age={22} gender={'male'} />
+      {/* <Navbar owner={user} message={'hello i am function based props'}></Navbar> */}
     </div>
   );
 }
--- firstapp/src/ClassAndFunctComponents.jsx
+++ firstapp/src/ClassAndFunctComponents.jsx
@@ -1,30 +1,45 @@
 import React from "react";
 
 
-// class Navbar extends React.Component{
-//     constructor(){
-//         super()
-//         this.name='johnny'
-//     }
-//     render(){
-//         return(
-//             <div>
-//                 <h1>this is class based component</h1>
-//                 <h2>{this.name}</h2>
-//             </div>
-//         )
-//     }
-// }
+class Navbar extends React.Component{
+    constructor(){
+        super()
+        this.name='jyotiprakash'
+    }
+    render(){
+        return(
+            <div>
+                <h1>this is class based component</h1>
+                {/* <h2>{this.name}</h2> */}
+                <h2>{this.props.name}</h2>
+                <h2>{this.props.age}</h2>
+                <h2>{this.props.gender}</h2>
 
-
-
-function Navbar(){
-    return(
-        <div>
-            <h1>This is function bassed component</h1>
-        </div>
-    )
+                <h2>Address:{this.props.add}</h2>
+            </div>
+        )
+    }
+}
+Navbar.defaultProps={
+    name:"name is not available",
+    
+    add:"Address is not available",
 }
 
 
-export default Navbar
+
+// const Navbar=(props)=>{
+//     console.log(props.owner)
+//     return(
+//         <div>
+//             <h1>{props.message}</h1>
+//             <h2>{props.owner.name}</h2>
+//             <h2>{props.owner.age}</h2>
+//             <h2>{props.owner.address}</h2>
+
+
+//         </div>
+//     )
+// }
+
+export default Navbar;
